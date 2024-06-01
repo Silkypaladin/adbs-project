f024632434d226d170558bccd68c04fc0423ef8a
Szymon Bednarski <bednarski.szymon99@gmail.com>
2024-06-01 16:08:50 +0200

    add users controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PostsModule } from "./posts/posts.module";
 import { ReactionsModule } from "./reactions/reactions.module";
 import { RelationshipsModule } from "./relationships/relationships.module";
+import { MongoModule } from "./database/mongo/mongo.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [PostsModule, ReactionsModule, RelationshipsModule],
+  imports: [PostsModule, ReactionsModule, RelationshipsModule, MongoModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
