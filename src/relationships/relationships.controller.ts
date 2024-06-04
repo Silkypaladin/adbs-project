966330a64c78f7ebe7b3c9b01e6d550d696bfd28
Szymon Bednarski <bednarski.szymon99@gmail.com>
2024-06-04 20:27:52 +0200

    add relationships controller

--- src/relationships/relationships.controller.ts
+++ src/relationships/relationships.controller.ts
@@ -1,8 +1,45 @@
-import { Controller } from "@nestjs/common";
-import { RelationshipsService } from "./relationships.service";
+import { Controller, Post, Body, Param, Get } from '@nestjs/common';
+import { RelationshipsService } from './relationships.service';
 
 @Controller('relationships')
 export class RelationshipsController {
-  constructor(private readonly relationshipsService: RelationshipsService) {
+  constructor(private readonly relationshipsService: RelationshipsService) {}
+
+  @Post('send-friend-request')
+  async sendFriendRequest(
+    @Body() sendFriendRequestDto: { fromUserId: string; toUserId: string },
+  ) {
+    const { fromUserId, toUserId } = sendFriendRequestDto;
+    return await this.relationshipsService.sendFriendRequest(
+      fromUserId,
+      toUserId,
+    );
+  }
+
+  @Post('accept-friend-request')
+  async acceptFriendRequest(
+    @Body() acceptFriendRequestDto: { fromUserId: string; toUserId: string },
+  ) {
+    const { fromUserId, toUserId } = acceptFriendRequestDto;
+    return await this.relationshipsService.acceptFriendRequest(
+      fromUserId,
+      toUserId,
+    );
+  }
+
+  @Post('reject-friend-request')
+  async rejectFriendRequest(
+    @Body() rejectFriendRequestDto: { fromUserId: string; toUserId: string },
+  ) {
+    const { fromUserId, toUserId } = rejectFriendRequestDto;
+    return await this.relationshipsService.rejectFriendRequest(
+      fromUserId,
+      toUserId,
+    );
+  }
+
+  @Get('suggest-friends/:userId')
+  async suggestFriends(@Param('userId') userId: string) {
+    return await this.relationshipsService.suggestFriends(userId);
   }
 }
